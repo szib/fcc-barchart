075e4d88c561b3b5f1b14d976a893a885a4f51a0
szib <szib@users.noreply.github.com>
2017-07-10 11:30:20 +0100

    Init

---
+++ react-ui/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div className="App">
+        React App
+      </div>
+    );
+  }
+}
+
+export default App;
