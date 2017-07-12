cf706e0f0e4f3bafd0ca6d3196fb8d62c9a6930d
szib <szib@users.noreply.github.com>
2017-07-12 12:28:15 +0100

    SVG chart

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -37,44 +37,100 @@ class App extends Component {
     if (this.state.data.length !== 0) {
 
       let data = this.state.data
+      let formatCurrency = d3.format("$,.2f");
+      let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
       let margin = {
-          top: 5,
-          right: 10,
+          top: 50,
+          right: 40,
           bottom: 30,
           left: 75
         }
 
       let width = 1000 - margin.left - margin.right
-      let height = 1800 - margin.top - margin.bottom
-      let barWidth = Math.ceil(width / data.length)
-      console.warn(width, height, data.length, barWidth);
+      let height = 600 - margin.top - margin.bottom
+      let barWidth = width / data.length
 
       let y = d3.scaleLinear()
-        .range([height, 0])
-        .domain([0, d3.max(data, function(d) {
-          return d[1];
-        })]);
+          .range([height, 0])
+          .domain([0, d3.max(data, d => d[1])])
 
-      d3.select('.App')
-      .append('div')
-      .attr('class', 'chart')
-      .style('height', x => height)
-      .style('width', x => width)
-      .style('background-color', '#dddddd')
+      let x = d3.scaleTime()
+          .range([0, width])
+          .domain([this.state.minDate, this.state.maxDate])
 
-      d3.select('.chart')
-      .selectAll('div')
-      .data(data)
-        .enter()
-        .append('div')
-        .attr("class", "bar")
-        .style("height", function(d) { return y(d[1]) + "px" })
-        .style("width", d => barWidth + "px")
+      let xAxis = d3.axisBottom()
+          .scale(x)
+
+      let yAxis = d3.axisLeft()
+          .scale(y)
+
+      let chart = d3.select('.chart')
+          .attr('height', height + margin.top + margin.bottom)
+          .attr('width', width + margin.left + margin.right)
+          .style('background-color', '#dddddd')
+        .append('g')
+          .attr('transform', 'translate(' + margin.left + "," + margin.top + ")" )
+
+      let div = d3.select('.container')
+          .append('div')
+          .attr('class', 'tooltip')
+          .style('opacity', 0)
+
+      let bar = chart.selectAll('.bar')
+          .data(data)
+          .enter()
+          .append('rect')
+          .attr('class', 'bar')
+          .attr('y', d => y(d[1]))
+          .attr('x', d => x(new Date(d[0])))
+          .attr("height", d => height - y(d[1]))
+          .attr("width", barWidth - 1)
+
+        bar.on('mouseover', d => {
+            var currentDateTime = new Date(d[0]);
+            div.transition()
+              .duration(200)
+              .style("opacity", 0.8);
+
+            div.html("<span class='amount'>" + formatCurrency(d[1]) + "&nbsp;Billion </span><br><span class='year'>" + currentDateTime.getFullYear() + ' - ' + months[currentDateTime.getMonth()] + "</span>")
+              .style("left", (d3.event.pageX + 10) + "px")
+              .style("top", (d3.event.pageY - 60) + "px");
+          })
+          .on('mouseout', d => {
+            div.transition()
+              .duration(500)
+              .style("opacity", 0);
+          })
+
+      chart.append('g')
+          .attr('class', 'x axis')
+          .attr('transform', 'translate(0,' + height +')')
+          .call(xAxis)
+
+      chart.append('g')
+          .attr('class', 'y axis')
+          .call(yAxis)
+        .append('text')
+          .attr('transform', 'rotate(-90)')
+          .attr('y',6)
+          .attr('dy', '0.71em')
+          .style('text-anchor', 'end')
+          .text('GDP in USD (billion)')
+
+      chart.append('g')
+          .attr('class', 'title')
+        .append('text')
+          .attr('y',0)
+          .attr('x',250)
+          .text('Gross Domestic Product')
     }
 
     return (
       <div className="App">
+        <div className="container">
+          <svg className="chart"></svg>
+        </div>
       </div>
     );
   }
